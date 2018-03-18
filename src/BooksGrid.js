e06f4936a786a22cf35602804d669b2a2238d673
Felipe Menezes <lipe.menezes@live.com>
2018-03-18 20:48:14 -0300

    feat: create bookgrid (container for books)

---
+++ src/BooksGrid.js
@@ -0,0 +1,19 @@
+import React from 'react'
+// import * as BooksAPI from './BooksAPI'
+import './App.css'
+import Book from './Book'
+
+class BookGrid extends React.Component {
+
+  render() {
+    return (
+      <ol className="books-grid">
+        <li>
+          <Book />
+        </li>
+      </ol>
+    )
+  }
+}
+
+export default BookGrid
